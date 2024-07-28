264deb621552a8c87951ceba2f2c6f29c4e834f0
Live session user <pop-os@pop-os>
2024-07-28 08:01:00 +0000

    comitei

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const { createServer } = require("node:http");
+const { join } = require("node:path")
+const { Server } = require("socket.io")
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+    res.sendFile(join(__dirname, "frontEnd/index.html"))
+})
+
+io.on("connection", (socket) => {
+    console.log('A user connected');
+    socket.on('chat message', (msg) => console.log("mensage:" + msg));
+    socket.on("disconnect", () => {
+        console.log("user disconnect");
+    })
+})
+
+server.listen(3000, () => (console.log("Server conected")));
+
